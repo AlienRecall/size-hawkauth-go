ffa5e0de98961ef907fc96eb55696fe178530642
AlienRecall <68950287+AlienRecall@users.noreply.github.com>
2020-11-27 03:06:05 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"crypto/hmac"
-	"crypto/rand"
-	"crypto/sha256"
-	b64 "encoding/base64"
-	"fmt"
-	"time"
+  "crypto/hmac"
+  "crypto/rand"
+  "crypto/sha256"
+  b64 "encoding/base64"
+  "fmt"
+  "time"
 )
 
 func gen(id string, method string, path string, domain string) string {
-	ts := time.Now().Unix()
-	randomBytes := make([]byte, 16)
-	rand.Read(randomBytes)
-	nonce := b64.StdEncoding.EncodeToString([]byte(randomBytes))
-	data := fmt.Sprintf(`"hawk.1.header\n%d\n%s\n%s\n%s\n%s\n80"`, ts, nonce, method, path, domain)
-	h := hmac.New(sha256.New, []byte("7fbda8fefe33eec041a3cbca366fab32"))
-	h.Write([]byte(data))
-	mac := b64.StdEncoding.EncodeToString([]byte(h.Sum(nil)))
-	header := fmt.Sprintf(`Hawk id="%s", mac="%s", ts="%d", nonce="%s"`, id, mac, ts, nonce)
-	return header
+  ts := time.Now().Unix()
+  randomBytes := make([]byte, 16)
+  rand.Read(randomBytes)
+  nonce := b64.StdEncoding.EncodeToString([]byte(randomBytes))
+  data := fmt.Sprintf(`"hawk.1.header\n%d\n%s\n%s\n%s\n%s\n80"`, ts, nonce, method, path, domain)
+  h := hmac.New(sha256.New, []byte("7fbda8fefe33eec041a3cbca366fab32"))
+  h.Write([]byte(data))
+  mac := b64.StdEncoding.EncodeToString([]byte(h.Sum(nil)))
+  header := fmt.Sprintf(`Hawk id="%s", mac="%s", ts="%d", nonce="%s"`, id, mac, ts, nonce)
+  return header
 }
 
 func main() {
@@ -28,5 +28,5 @@ func main() {
   path := "/stores/size/carts?channel=android-app-phone"
   domain := "prod.jdgroupmesh.cloud"
   header := gen(sizeID, method, path, domain)
-	fmt.Println(header)
+  fmt.Println(header)
 }
