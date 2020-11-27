7df6ab6bc0877a0a8842f5171f81bc938aa037d0
AlienRecall <68950287+AlienRecall@users.noreply.github.com>
2020-11-27 11:34:21 +0100

    Update main.go

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
   "time"
 )
 
-func gen(id string, method string, path string, domain string) string {
+func gen(id string, secret string, method string, path string, domain string) string {
   ts := time.Now().Unix()
   randomBytes := make([]byte, 16)
   rand.Read(randomBytes)
   nonce := b64.StdEncoding.EncodeToString([]byte(randomBytes))
   data := fmt.Sprintf(`"hawk.1.header\n%d\n%s\n%s\n%s\n%s\n80"`, ts, nonce, method, path, domain)
-  h := hmac.New(sha256.New, []byte("7fbda8fefe33eec041a3cbca366fab32"))
+  h := hmac.New(sha256.New, []byte(secret))
   h.Write([]byte(data))
   mac := b64.StdEncoding.EncodeToString([]byte(h.Sum(nil)))
   header := fmt.Sprintf(`Hawk id="%s", mac="%s", ts="%d", nonce="%s"`, id, mac, ts, nonce)
@@ -24,9 +24,10 @@ func gen(id string, method string, path string, domain string) string {
 
 func main() {
   sizeID := "c012543e9e"
+  sizeSecret := "7fbda8fefe33eec041a3cbca366fab32"
   method := "GET"
   path := "/stores/size/carts?channel=android-app-phone"
   domain := "prod.jdgroupmesh.cloud"
-  header := gen(sizeID, method, path, domain)
+  header := gen(sizeID, sizeSecret, method, path, domain)
   fmt.Println(header)
 }
